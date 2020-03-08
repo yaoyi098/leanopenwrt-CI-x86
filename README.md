3cce447f3bf91a18ed54f0a37bf94e1753c0a006
Yao Yimeng <yym900902@gmail.com>
2020-03-08 17:36:36 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# leanopenwrt-CI-x86
+# Actions-OpenWrt
+
+[![LICENSE](https://img.shields.io/github/license/mashape/apistatus.svg?style=flat-square&label=LICENSE)](https://github.com/yaoyi098/leanopenwrt-CI-Raspi3Bp/blob/master/LICENSE)
+[![GitHub Stars](https://img.shields.io/github/stars/yaoyi098/leanopenwrt-CI-Raspi3Bp.svg?style=flat-square&label=Stars&logo=github)](https://github.com/yaoyi098/leanopenwrt-CI-Raspi3Bp/stargazers)
+[![GitHub Forks](https://img.shields.io/github/forks/yaoyi098/leanopenwrt-CI-Raspi3Bp.svg?style=flat-square&label=Forks&logo=github)](https://github.com/yaoyi098/leanopenwrt-CI-Raspi3Bp/fork)
+
+Build OpenWrt using GitHub Actions
+
+## Downloads
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/yaoyi098/leanopenwrt-CI-Raspi3Bp?style=for-the-badge&label=Download)](https://github.com/yaoyi098/leanopenwrt-CI-Raspi3Bp/releases/latest)
+
+## License
+
+[MIT](https://github.com/P3TERX/Actions-OpenWrt/blob/master/LICENSE) © P3TERX
+
---
+++ diy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.254/g' package/base-files/files/bin/config_generate
+
+# Diasble dhcp for lan
+sed -i '29a\\toption\ ignore\ 1' package/network/services/dnsmasq/files/dhcp.conf
+
+# Modify the version number
+sed -i 's/OpenWrt/YYM build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Add kernel build user
+[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_USER="yym"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"yym"@' .config
+
+# Add kernel build domain
+[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
+    
